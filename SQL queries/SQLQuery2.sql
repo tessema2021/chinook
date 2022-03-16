6e51b1847deb5ac5287e2d13cfb0ec609a560d41
tessema2021 <ninafuchere@gmail.com>
2022-03-16 03:04:16 -0500

    clear unwanted code

---
+++ SQL queries/SQLQuery2.sql
@@ -0,0 +1,21 @@
+﻿---1--
+Select FirstName,LastName,CustomerId,Country
+From Customer
+Where Country <> 'Us'
+----2---
+Select *
+From Customer
+Where Country = 'Brazil'
+---3--
+Select c.FirstName,c.LastName,i.InvoiceId,i.InvoiceDate,i.BillingCountry
+From Invoice i
+left Join Customer c on c.CustomerId = i.CustomerId
+Where Country ='Brazil'
+--4--
+Select *
+From Employee
+where Title = 'Sales Support Agent'
+----5--
+Select distinct BillingCountry
+From Invoice
+----6---
